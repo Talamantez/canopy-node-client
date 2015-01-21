15969b9d22ae2d41f149770616a51b421772ef36
Talamantez <robert.talamantez2@gmail.com>
2015-01-21 13:59:43 -0800

    Repo cleanup

--- canopy.js
+++ canopy.js
@@ -1,14 +1,16 @@
-// canopy.device(ID).set("beta_waves", 4.5);
 var http = require('http');
+
 var Device = function () {
+
 	this.id = function(id){
 			this.id = id
 	}
+
 	this.auth = function(authString){
-			//9fcd9f2e-9955-439e-b05d-3a8171676661:ddkgPG1jqntzhf8lJagVOoZvcc+mMOl8			
 			var myAuthString = new Buffer(authString).toString("base64");
 			this.auth = 'Basic ' + myAuthString;
 		}
+
 	this.set = function(param, value){
 			console.log('Posting data to device: '+ this.id);
 			var sddlType = "out float32 " + param;
@@ -20,13 +22,6 @@ var Device = function () {
 			};
 			payload.sddl[sddlType] = {};
 			payload.vars[param] = value;
-/*			var payload = {
-			    "sddl": { "out float32 beta_waves" : {},
-			        },
-			    "vars": {
-			            "beta_waves": value
-			            }				
-			}*/
 			console.log('payload: ')
 			console.dir(payload);
 			var payloadString = JSON.stringify(payload);
@@ -44,6 +39,7 @@ var Device = function () {
 				method: 'POST',
 				headers: headers
 			}
+
 			var req = http.request(options, function(res) {
 			  console.log(options);
 			  console.log('res.setEncoding');
@@ -71,6 +67,7 @@ var Device = function () {
 
 					},
 	this.get = function(){
+
 			console.log('Getting data for device: '+this.id);
 			var options = {
 			  host: 'sandbox.canopy.link',
@@ -80,12 +77,10 @@ var Device = function () {
 			callback = function(response) {
 			  var str = '';
 
-			  //another chunk of data has been recieved, so append it to `str`
 			  response.on('data', function (chunk) {
 			    str += chunk;
 			  });
 
-			  //the whole response has been recieved, so we just print it out here
 			  response.on('end', function () {
 			    console.log(str);
 			  });
@@ -97,7 +92,5 @@ var Device = function () {
 				
 
 }
-module.exports.Device = new Device();
 
-/*var myDevice = new canopy.Device();
-myDevice.id(1234);*/
+module.exports.Device = new Device();
